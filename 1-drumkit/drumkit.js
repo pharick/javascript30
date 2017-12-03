24d6b9a5ef06f5cfcbc58294362ec4d4e2abdf93
pharick <artemforkunov@gmail.com>
2017-12-04 00:16:57 +0300

    Drumkit refactoring

--- 1-drumkit/drumkit.js
+++ 1-drumkit/drumkit.js
@@ -1,15 +1,24 @@
-window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+function playSound(keyCode) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
+}
 
-  const item = document.querySelector(`.drumkit__item[data-key="${e.keyCode}"]`);
+function pickUpItem(keyCode) {
+  const item = document.querySelector(`.drumkit__item[data-key="${keyCode}"]`);
   item.classList.add('drumkit__item_pressed');
+}
+
+function pickDownItem(e) {
+  if (e.propertyName != 'transform') return;
+  e.target.classList.remove('drumkit__item_pressed');
+}
+
+window.addEventListener('keydown', function(e) {
+  playSound(e.keyCode);
+  pickUpItem(e.keyCode);
 });
 
 const items = document.querySelectorAll('.drumkit__item');
-items.forEach(item => item.addEventListener('transitionend', function(e) {
-  if (e.propertyName != 'transform') return;
-  item.classList.remove('drumkit__item_pressed');
-}));
+items.forEach(item => item.addEventListener('transitionend', pickDownItem));
