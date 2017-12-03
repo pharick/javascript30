1c9e317b7768f3e35407f15e71a14a935e8a8fd4
pharick <artemforkunov@gmail.com>
2017-12-04 01:18:40 +0300

    Click on items of drumkit

--- 1-drumkit/drumkit.js
+++ 1-drumkit/drumkit.js
@@ -1,12 +1,12 @@
 function playSound(keyCode) {
-  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  var audio = document.querySelector(`audio[data-key="${keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
 }
 
 function pickUpItem(keyCode) {
-  const item = document.querySelector(`.drumkit__item[data-key="${keyCode}"]`);
+  var item = document.querySelector(`.drumkit__item[data-key="${keyCode}"]`);
   item.classList.add('drumkit__item_pressed');
 }
 
@@ -20,5 +20,19 @@ window.addEventListener('keydown', function(e) {
   pickUpItem(e.keyCode);
 });
 
-const items = document.querySelectorAll('.drumkit__item');
-items.forEach(item => item.addEventListener('transitionend', pickDownItem));
+var drumkit = document.querySelector('.drumkit');
+
+drumkit.addEventListener('click', function(e) {
+  var target = e.target;
+
+  while (target != drumkit) {
+    if (target.classList.contains('drumkit__item')) {
+      playSound(target.dataset.key);
+      pickUpItem(target.dataset.key);
+      return;
+    }
+    target = target.parentNode;
+  }
+});
+
+drumkit.addEventListener('transitionend', pickDownItem);
